ea14fdff7490d4fe94c700fa7962a7c18ef4ede9
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-08-22 12:26:39 -0400

    Form formatted

--- app/controllers/new_table.rb
+++ app/controllers/new_table.rb
@@ -11,4 +11,20 @@ class Serve
         @@db.create_table(title, scm)
     end
     self.create_table if @@env["REQUEST_METHOD"] == "POST"
+
+    def self.build_form(n)
+        res = ""
+        for i in 1..n do
+            res << "<b>Column #{i}:</b>"
+            res << <<-CMD
+                name: <input type="text" name="col#{i}"> type: <input type="text" name="type#{i}">
+            CMD
+            res << "<br/>"
+        end
+        res
+    end
+
+    def self.ping(res)
+        res
+    end
 end
